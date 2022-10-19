1e079ca72ed6f6db96145cd04a98c2fe4e5bdca5
Epaphras Kyomnom Peter <epaphraspeter76@gmail.com>
2022-10-19 16:05:19 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,12 @@ _putchar(op + 48);
 }
 else if (op <= 99)
 {
+_putchar(32);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
+}
+else 
+{
 _putchar(((op / 100) % 10) + 48);
 _putchar(((op / 10) % 10) + 48);
 _putchar((op % 10) + 48);
